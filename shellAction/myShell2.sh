241a729fef5928ec1e201a0a9811eb3badbb90ce
bruno boutin <b.boutin@uva.nl>
2021-04-29 11:16:20 +0200

    Update myShell2.sh

--- shellAction/myShell2.sh
+++ shellAction/myShell2.sh
@@ -1,1 +1,4 @@
-echo "HALLO SHELL $1!!"
+echo "HALLO SHELL2"
+curl -s -X POST https://api.github.com/repos/$OWNER/$REPO/issues/$ISSUE_NUMBER/comments \
+  -d '{"body":"Hello, thanks for your issue!"}' \
+  -H "Authorization: Bearer $TOKEN"
