266b9952c48f7ccb896237aabab4bec33e201b7f
bruno boutin <b.boutin@uva.nl>
2021-04-30 13:40:41 +0200

    Update cliShell.sh

--- .github/cliShell.sh
+++ .github/cliShell.sh
@@ -1,4 +1,9 @@
 owner_repo="$1"
 author='weblate'
-cli_reply=$(gh pr list -R ${owner_repo})
+cli_reply=$(gh pr list -R $owner_repo)
 echo "$cli_reply"
+first_line=$(echo "$test" | sed -n '1p')
+echo "$first_line"
+pr_number=$(echo "$first_line" | grep -oE '^[0-9]+')
+echo "$pr_number"
+gh pr merge --squash $pr_number -R $owner_repo
